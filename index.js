9b920cf1a68ef31a419a4a789f69f9fea764fbdd
Jherich Silas <jherichsilas@Jherichs-MacBook-Pro-7.local>
2020-02-17 03:37:15 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const apiKey = 'a600403bd1984d1fb0b0516940b2f090'
+
+const searchURL = 'https://newsapi.org/v2/everything';
+
+
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params)
+    .map(key => `${key}=${params[key]}`)
+  return queryItems.join('&');
+}
+
+function getNews(query, maxResults=10) {
+  const params = {
+    q: query,
+    language: "en",
+  };
+  const queryString = formatQueryParams(params)
+  const url = searchURL + '?' + queryString;
+
+  console.log(url);
+  
+  const options = {
+    headers: new Headers({
+      "X-Api-Key": apiKey})
+  };
+
+  fetch(url, options)
+    .then(response => response.json())
+    .then(responseJson => console.log(responseJson))
+    .catch(err => {
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
+}
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    const searchTerm = $('#js-search-term').val();
+    const maxResults = $('#js-max-results').val();
+    getNews(searchTerm, maxResults);
+  });
+}
+
+$(watchForm);
